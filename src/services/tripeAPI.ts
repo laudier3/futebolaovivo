5bb2d233292c2c933ef67d83d01da4bf4dadf7a0
Laudier2 <laudiersantanamei@gmail.com>
2025-07-13 09:09:57 -0300

    Remove Stripe test key

--- src/services/tripeAPI.ts
+++ src/services/tripeAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4242',
+  baseURL: 'app4.apinonshops.store',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -9,7 +9,7 @@ const api = axios.create({
 
 
 export async function createCheckout(email: string) {
-  const response = await axios.post(`http://localhost:4242/create-checkout-session`, {
+  const response = await axios.post(`app4.apinonshops.store/create-checkout-session`, {
     email,
   });
   console.log('E-mail enviado para pagamento:', email);
@@ -17,26 +17,3 @@ export async function createCheckout(email: string) {
 }
 
 export { api }
-
-
-/*import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'http://localhost:4242',
-});
-
-export const createCheckout = async (email: string) => {
-  const response = await api.post('/create-checkout-session', { email });
-  return response.data;
-};
-
-/*import axios from 'axios';
-
-const api = axios.create({
-  baseURL: "http://localhost:4242",
-});
-
-export const createCheckout = async (email: string) => {
-  const response = await api.post('/create-checkout-session', { email });
-  return response.data;
-};*/
