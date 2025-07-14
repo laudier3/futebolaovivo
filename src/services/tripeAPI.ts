55239f12d9fd61ecf8dc0472f51cd627d62d5038
Laudier2 <laudiersantanamei@gmail.com>
2025-07-13 23:08:35 -0300

    Remove Stripe test key

--- src/services/tripeAPI.ts
+++ src/services/tripeAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'app4.apinonshops.store',
+  baseURL: 'http://localhost:4242',
   headers: {
     'Content-Type': 'application/json',
   },
