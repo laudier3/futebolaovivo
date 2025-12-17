c2e73c8400e393a4ede64f4f52d018788856d5a4
root <root@hk.hk>
2025-12-17 10:49:13 -0300

    O README FOI ATUALIZADO

--- src/services/tripeAPI.ts
+++ src/services/tripeAPI.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://futebol.urlcurt.site',
+  baseURL: 'http://localhost:4242', //'https://futebol.urlcurt.site',
   headers: {
     'Content-Type': 'application/json',
   },
